99e6e0ebae2e858c5fb230cdeb97e6dd35196bdd
Dave Hoffman <spaghettidave80@gmail.com>
2018-09-18 19:41:29 -0400

    Search page working correctly.

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -7,22 +7,39 @@ import * as BooksAPI from './BooksAPI'
 class SearchPage extends Component {
 
   state = {
-    bookQueryResults: []
+    bookQueryResults: [],
+    query: ''
   }
 
-  serializeSearchForm = (e) => {
+  searchForBooks = (event) => {
+    console.log('event.target.value: ', event.target.value)
+    if(event.target.value.length > 0) {
+      BooksAPI.search(event.target.value).then((responce) => {
+        this.setState({ bookQueryResults: responce})
+      }).catch((error) => {
+        console.log('error: ', error)
+      })
+    } else {
+      this.setState({ bookQueryResults: [] })
+    }
+  }
+
+  updateQuery = (event) => {
+    let query = event.target.value
+    this.setState({ query })
+    this.searchForBooks(event)    
+  }
+
+  handleSubmit = (e) => {
     e.preventDefault()
-    const values = serializeForm(e.target, { hash: true })
-    this.searchForBooks(values.apiQuery)
-  }
-
-  searchForBooks = (query) => {
-    BooksAPI.search(query).then((responce) => {
-      this.setState({ bookQueryResults: responce})
-      console.log('bookQueryResults: ', this.state.bookQueryResults)
-    }).catch((error) => {
-      console.log(error)
-    })
+    const serializedQueryValue = serializeForm(e.target, { hash: true })
+    if(serializedQueryValue.apiQuery.length > 0 || serializedQueryValue.api == undefined) {      
+      BooksAPI.search(serializedQueryValue.apiQuery).then((responce) => {
+        this.setState({ bookQueryResults: responce })
+      }).catch((error) => {
+        console.log(error)
+      })
+    }
   }
 
   render() {
@@ -31,18 +48,29 @@ class SearchPage extends Component {
         <div className='search-books-bar'>
           <Link to='/' className='close-search'>Close</Link>
           <div className='search-books-input-wrapper'>
-            <form onSubmit={this.serializeSearchForm} className='search-for-a-book'>
-              <input type='text' placeholder='Search by title or author' name='apiQuery'/>
+            <form onSubmit={this.handleSubmit} className='search-for-a-book'>
+              <input
+                type='text'
+                placeholder='Search by title'
+                name='apiQuery'
+                value={this.state.query}
+                onChange={(event) => this.updateQuery(event)}
+              />
             </form>
           </div>
         </div>
         <div className='search-books-results'>
           <ol className='books-grid'>
-            {this.state.bookQueryResults.map((book) => (
+            {this.state.bookQueryResults !== undefined && this.state.bookQueryResults.length > 1 ? (this.state.bookQueryResults.map((book) => (
               <li key={book.id}>
                 <div className='book'>
                   <div className='book-top'>
-                    <div className='book-cover' style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})`}}></div>
+                    {book.imageLinks !== undefined ? (
+                      <div className='book-cover' style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})`}}></div>
+                    ) : (
+                      <div className='book-cover'>Sorry but there's no cover photo for this book.</div>
+                    )}
+                    
                     <div className='book-shelf-changer'>
                       <select onChange={(event) => this.props.moveBook(book, event.target.value)} value="currentlyReading">
                         <option value="move" disabled>Move to...</option>
@@ -54,10 +82,18 @@ class SearchPage extends Component {
                     </div>
                   </div>
                   <div className='book-title'>{book.title}</div>
-                  <div className='book-authors'>{book.authors}</div>
+                  {book.authors !== undefined ? (
+                    <div className='book-authors'>{book.authors}</div>
+                  ) : (
+                    <div className='book-authors'>Unknown author(s)</div>
+                  )}
                 </div>
               </li>
-            ))}
+            ))) : (
+              <li key='no-books-found'>
+                <p className='no-books'>Sorry, but there are no books that match your search.</p>
+              </li>
+            )}
           </ol>
         </div>
       </div>
